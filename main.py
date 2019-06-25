71a96e4aa9546951f407c9b93dca9d1ffc2ba9a7
javag97 <javier.garcia549@gmail.com>
2019-06-25 14:36:06 -0700

    tacos

--- main.py
+++ main.py
@@ -2,8 +2,29 @@
 
 def hello_world():
 
+
+    print("2 lb flat steak hanger steak
+4 Clove's garlic minced
+1/2 cup cilantro
+1 lime zest
+Juice of one lime
+Juice of one orange 
+Teaspoon of cumin 
+Salt and pepper to taste
+1/2 minced onion
+3/4 cup olive oil 
+A teaspoon of apple cider vinegar
+
+Mix marinade
+Steak in marinade  90mins- up to 4hours
+Grill on medium-high heat 4-6 mins per side")
+
+
     x = 2 + 2
 
     print("This is broken, ha ha ha (evil laugh)")
 
 
+        print(x+4)
+
+        
