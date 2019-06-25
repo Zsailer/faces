283c306f474652e4f4075e9c733a483f0886b3d2
Markelle Kelly <markellekelly@gmail.com>
2019-06-25 14:37:24 -0700

    added features

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def hello_world():
 
     x = 2 + 2
 
-    print("This is broken, ha ha ha (evil laugh)")
+    print("hey girl")
 
+    for i in range(5):
+        print("testing")
+        if i == 2:
+            print("i is 2!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(x+4)
 
